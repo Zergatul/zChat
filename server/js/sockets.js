10b83ee73e97d46f8eaa959ff80d82e9a381adaf
Zergatul <z3rgatul@gmail.com>
2013-10-18 19:23:48 +0300

    Chat interface implementation

--- server/js/sockets.js
+++ server/js/sockets.js
@@ -1,9 +1,32 @@
 (function () {
 
+	var pck = { srv: {}, cl: {} };
+	pck.cl.nick = 1;
+	pck.cl.chatInvite = 2;
+	pck.cl.acceptInvite = 3;
+	pck.cl.declineInvite = 4;
+	pck.cl.message = 5;
+	pck.srv.nickResponse = 1;
+	pck.srv.otherUserChatInvite = 2;
+	pck.srv.chatInviteResponse = 3;
+	pck.srv.userInviteResponse = 4;
+	pck.srv.sessionInit = 5;
+	pck.srv.message = 6;
+
+	var okMessage = 'ok';
+
 	window.Connection = function () {
+		var self = this;
+
+		this._handlers = {};
+
 		this._ws = new WebSocket('ws://localhost:89/');
 		this._ws.onopen = function () {
 			console.log('Socket opened!');
+			if (typeof self._onConnect == 'function') {
+				self._onConnect();
+				delete self._onConnect;
+			}
 		};
 		this._ws.onclose = function (event) {
 			if (event.wasClean)
@@ -13,11 +36,94 @@
 			console.log('Code: ' + event.code + ' Reason: ' + event.reason);
 		};
 		this._ws.onmessage = function (event) {
-			console.log('Got data: ' + event.data);
+			var delimiterIndex = event.data.indexOf(':');
+			var id = event.data.substring(0, delimiterIndex);
+			id = parseInt(id);
+			var data = event.data.substring(delimiterIndex + 1);
+			resolveSrvPacket(self, id, data);
 		};
 		this._ws.onerror = function (event) {
 			console.log('Error: ' + event.message);
 		};
+	};
+
+	var setupHandler = function (con, id, callback, permanent) {
+		con._handlers[id] = { onMessage: callback };
+		if (permanent)
+			con._handlers[id].permanent = true;
+	};
+
+	var setupHandlers = function (con, id, onSuccess, onFail, permanent) {
+		con._handlers[id] = { onSuccess: onSuccess, onFail: onFail };
+		if (permanent)
+			con._handlers[id].permanent = true;
+	};
+
+	var clearHandlers = function (con, id) {
+		if (con._handlers[id])
+			delete con._handlers[id];
+	};
+
+	var resolveSrvPacket = function (con, id, data) {
+		if (con._handlers[id] != undefined) {
+			var handlers = con._handlers[id];
+			if (!handlers.permanent)
+				delete con._handlers[id];
+			if (handlers.onMessage != undefined)
+				handlers.onMessage(data);
+			else
+				if (data == okMessage)
+					handlers.onSuccess();
+				else
+					handlers.onFail(data);
+		} else
+			console.log('Packet without handler! id=' + id + '; data=' + data);
+	};
+
+	window.Connection.prototype.close = function () {
+		this._ws.close();
+	};
+
+	window.Connection.prototype.sendNick = function (nick, onSuccess, onFail) {
+		this._ws.send(pck.cl.nick + ':' + nick);
+		setupHandlers(this, pck.srv.nickResponse, onSuccess, onFail);
+	};
+
+	window.Connection.prototype.inviteForChatting = function (partnerNick, onSuccess, onFail) {
+		this._ws.send(pck.cl.chatInvite + ':' + partnerNick);
+		setupHandlers(this, pck.srv.chatInviteResponse, onSuccess, onFail);
+	};
+
+	window.Connection.prototype.waitForInviteResponse = function (onSuccess, onFail) {
+		setupHandlers(this, pck.srv.userInviteResponse, onSuccess, onFail);
+	};
+
+	window.Connection.prototype.acceptInvite = function () {
+		this._ws.send(pck.cl.acceptInvite + ':');
+	};
+
+	window.Connection.prototype.declineInvite = function () {
+		this._ws.send(pck.cl.declineInvite + ':');
+	};
+
+	window.Connection.prototype.sendMessage = function (text) {
+		this._ws.send(pck.cl.message + ':' + text);
+	};
+
+	window.Connection.prototype.onConnect = function (func) {
+		this._onConnect = func;
+	};
+
+	window.Connection.prototype.onChatRequest = function (func) {
+		setupHandler(this, pck.srv.otherUserChatInvite, func, true);
+	};
+
+	window.Connection.prototype.onSessionInit = function (func) {
+		setupHandler(this, pck.srv.sessionInit, func, true);
+	};
+
+	window.Connection.prototype.onMessage = function (func) {
+		setupHandler(this, pck.srv.message, func, true);
 	};
 
 })();
--- server/js/zc.js
+++ server/js/zc.js
@@ -1,21 +1,182 @@
 $(function () {
 
-	var con;
+	var connection;
+	var timer;
+	var nick, partnerNick;
+
+	window.secretKey = [201, 120, 8, 17, 30, 41, 109, 93, 196, 252, 55, 250, 36, 16, 101, 144];
 
 	$('#connect-btn').click(function () {
-		var nick = $('#nick-input').val();
+		nick = $('#nick-input').val();
 		if (nick.length == 0) {
 			alert('Empty nick not allowed');
 			return;
 		}
 
 		$('#connect-div').hide();
+		$('#connection-process-div').show();
 
-		con = new Connection();
-		con.sendNick(nick);
+		connection = new Connection();
+
+		connection.onConnect(function () {
+			connection.sendNick(nick, function () {
+				$('#connection-process-div').hide();
+				$('#choose-partner-div').show();
+				$('#nick-h4').text('Your nick: ' + nick);
+			}, function (msg) {
+				connection.close();
+				alert(msg);
+				$('#connection-process-div').hide();
+				$('#connect-div').show();
+			});
+		});
+
+		connection.onChatRequest(function (partnerNick) {
+			$('#choose-partner-div').hide();
+			$('#choose-partner-response-div').show();
+			$('#chat-inviting-p').text(partnerNick + ' is inviting you for chatting.');
+
+			var maxTime = 300;
+			var startTime = new Date();
+			var bar = $('#choose-partner-response-div .progress-bar');
+			bar.attr('aria-valuenow', maxTime);
+			bar.css('width', '100%');
+			var timerFunction = function () {
+				var time = maxTime - Math.round((new Date() - startTime) / 100);
+				if (time <= 0)
+					clearInterval(timer);
+				bar.attr('aria-valuenow', time);
+				bar.css('width', Math.round(400 * time / maxTime) + 'px');
+			};
+			timer = setInterval(timerFunction, 100);
+		});
+
+		connection.onSessionInit(function (_partnerNick) {
+			partnerNick = _partnerNick;
+			$('#session-init-div').hide();
+			$('#chat-div').show();
+		});
+
+		connection.onMessage(function (message) {
+			addMessage('panel-warning', partnerNick, decrypt(message));
+		});
 	});
+
+	var addMessage = function (cssClass, title, body) {
+		var msgDiv = $('#chat-div > div:first').clone();
+		msgDiv.removeClass('panel-primary');
+		msgDiv.addClass(cssClass);
+		msgDiv.find('.panel-title').text(title);
+		msgDiv.find('.panel-body').text(body);
+		msgDiv.insertBefore($('#chat-div > div:last'));
+	};
 
 	$('#random-nick-btn').click(function () {
-		$('#nick-input').val('anon' + Math.floor(1000000 * Math.random()));
+		$('#nick-input').val('anon' + Math.floor(100000 + 900000 * Math.random()));
 	});
+
+	$('#chat-invite-btn').click(function () {
+		partnerNick = $('#partner-input').val();
+		if (partnerNick.length == 0) {
+			alert('Empty nick not allowed');
+			return;
+		}
+		$('#choose-partner-div').hide();
+		$('#choose-partner-request-div').show();
+
+		connection.inviteForChatting(partnerNick, function () {
+			var maxTime = 300;
+			var startTime = new Date();
+			var bar = $('#choose-partner-request-div .progress-bar');
+			bar.attr('aria-valuenow', maxTime);
+			bar.css('width', '100%');
+			var gotResponse = false;
+			var timerFunction = function () {
+				var time = maxTime - Math.round((new Date() - startTime) / 100);
+				if (time <= 0) {
+					clearInterval(timer);
+					$('#choose-partner-request-div').hide();
+					$('#choose-partner-div').show();		
+					return;
+				}
+				if (gotResponse) {
+					clearInterval(timer);
+					return;
+				}
+				bar.attr('aria-valuenow', time);
+				bar.css('width', Math.round(400 * time / maxTime) + 'px');
+			};
+			timer = setInterval(timerFunction, 100);
+
+			connection.waitForInviteResponse(function () {
+				gotResponse = true;
+
+				$('#choose-partner-request-div').hide();
+				$('#session-init-div').show();
+			}, function (msg) {
+				gotResponse = true;
+
+				$('#choose-partner-request-div').hide();
+				$('#choose-partner-div').show();
+				alert(msg);
+			});
+		}, function (msg) {
+			alert(msg);
+			$('#choose-partner-request-div').hide();
+			$('#choose-partner-div').show();
+		});
+	});
+
+	$('#accept-invite-btn').click(function () {
+		connection.acceptInvite();
+		clearInterval(timer);
+		$('#choose-partner-response-div').hide();
+		$('#session-init-div').show();
+	});
+
+	$('#decline-invite-btn').click(function () {
+		connection.declineInvite();
+		clearInterval(timer);
+		$('#choose-partner-response-div').hide();
+		$('#choose-partner-div').show();
+	});
+
+	$('#send-btn').click(sendMessage);
+
+	$('#text-input-div input').on('keypress', function (e) {
+		if (e.which == 13)
+			sendMessage();
+	});
+
+	var sendMessage = function () {
+		var text = $('#text-input-div input').val();
+		if (text.length == 0)
+			return;
+		$('#text-input-div input').val('');
+		connection.sendMessage(encrypt(text));
+		addMessage('panel-success', nick, text);
+	}
+
+	var encrypt = function (text) {
+		var bytes = bh.stringToByteArray(text + '.');
+		bh.paddings.zero.pad(bytes, 16);
+		var encBytes = [];
+		while (bytes.length != 0) {
+			var block = bytes.splice(0, 16);
+			encBytes = encBytes.concat(aes.encrypt(block, window.secretKey));
+		}
+		return bh.byteArrayToHex(encBytes);
+	};
+
+	var decrypt = function (text) {
+		var bytes = bh.hexToByteArray(text);
+		var decBytes = [];
+		while (bytes.length != 0) {
+			var block = bytes.splice(0, 16);
+			decBytes = decBytes.concat(aes.decrypt(block, window.secretKey));
+		}
+		bh.paddings.zero.unpad(decBytes);
+		return bh.byteArrayToString(decBytes);
+	};
+
 });
