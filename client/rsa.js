a597cd75843cd4883359e4f2efc21ca4331416aa
Zergatul <z3rgatul@gmail.com>
2013-11-01 10:24:14 +0200

    Fix using OAEP

--- client/rsa.js
+++ client/rsa.js
@@ -7,12 +7,15 @@
 		this.publicKey = null;
 		this.privateKey = null;
 		this.keyLength = 0;
+		this.messageLength = 0;
 	};
 
 	window.rsa = {};
 
-	window.rsa.generateKeys = function (keyLength) {
-		if (typeof keyLength != 'number' || keyLength < 16)
+	// keyLength in bits
+	// messageLength in bytes
+	window.rsa.generateParameters = function (keyLength, messageLength) {
+		if (typeof keyLength != 'number' || keyLength < 16 || typeof messageLength != 'number')
 			throw 'Invalid parameter';
 
 		// generate primes, and check for difference must be > 2^(keyLength / 4)
@@ -33,6 +36,7 @@
 
 		var result = new RSAParameters();
 		result.keyLength = keyLength;
+		result.messageLength = messageLength;
 		result.publicKey = { n: n, e: e };
 		result.privateKey = { n: n, d: d };
 		return result;
@@ -43,13 +47,13 @@
 			message = new Uint8Array(message);
 		if (!(message instanceof Uint8Array) || !(params instanceof RSAParameters))
 			throw 'Invalid parameters';
-		if (message.length != 16)
-			throw 'Not implemented';
+		if (message.length != params.messageLength)
+			throw 'Invalid data length';
 
 		if (message.length > params.keyLength / 8)
 			throw 'Cannot encrypt message which is greater than key';
 
-		var oaep = new OAEP(params.keyLength >> 3, 16, 32);
+		var oaep = new OAEP(Math.ceil(params.publicKey.n.bitLength() / 8) - 1, params.messageLength, 32);
 		var data = oaep.encode(message, random.SHA2PRNG);
 
 		return BigInt.fromUint8Array(data).modPow(params.publicKey.e, params.publicKey.n).toUint8Array();
@@ -63,8 +67,8 @@
 
 		var data = BigInt.fromUint8Array(cypher).modPow(params.privateKey.d, params.privateKey.n);
 
-		var oaep = new OAEP(params.keyLength >> 3, 16, 32);
-		var message = oaep.decode(data.toUint8Array(params.keyLength >> 3));
+		var oaep = new OAEP(Math.ceil(params.publicKey.n.bitLength() / 8) - 1, params.messageLength, 32)
+		var message = oaep.decode(data.toUint8Array(oaep._n));
 
 		return message;
 	};
